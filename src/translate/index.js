c9c6003f5ca040be6afb5dc6b08deb57cf2ce519
Michelle Guo <myguo@andrew.cmu.edu>
2025-11-12 00:21:01 -0500

    Update index.js - trying http://128.2.221.72:5000

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,29 +4,14 @@
 
 const translatorApi = module.exports;
 
-// translatorApi.translate = function (postData) {
-// 	// Returns [isEnglish, translatedContent]
-// 	// For testing: Detect if post contains common non-English words
-// 	// In production, this should call your actual translator API
-	
-// 	const content = postData.content || '';
-// 	const nonEnglishKeywords = ['hola', 'bonjour', 'hallo', 'ciao', 'olá', 'привет', '你好', 'こんにちは'];
-	
-// 	// Check if content contains any non-English keywords
-// 	const isEnglish = !nonEnglishKeywords.some(keyword =>
-// 		content.toLowerCase().includes(keyword.toLowerCase()));
-	
-// 	// For testing: provide a sample translation
-// 	const translatedContent = isEnglish ? '' :
-// 		'[TRANSLATED] This is a sample translation. Connect your translator API to get real translations.';
-	
-// 	return [isEnglish, translatedContent];
-// };
-
 translatorApi.translate = async function (postData) {
-//  Edit the translator URL below
- const TRANSLATOR_API = "http://translator:5000"
- const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
- const data = await response.json();
- return [data.is_english, data.translated_content];
+	const TRANSLATOR_API = 'http://128.2.221.72:5000';
+	try {
+		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+		const data = await response.json();
+		return [data.is_english, data.translated_content];
+	} catch (e) {
+		// If the API is down or any error occurs, return false and the original postData
+		return [false, postData];
+	}
 };
